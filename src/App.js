447268a78c982ef7a2f6d3e27cb6488114588184
Mikegv <xmik3yvx@live.com>
2022-04-26 13:26:28 -0700

    feat(gallery):started gallery section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Gallery from './components/gallery/Gallery';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/gallery/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Gallery.css'
+
+const Gallery = () => {
+    return (
+        <div className='gallery' id="gallery">
+            <h1 className='sectionTitle'>Gallery</h1>
+        </div>
+    );
+};
+
+export default Gallery;
