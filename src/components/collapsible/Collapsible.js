c37d6bc7749c2349c1cd0eeb8c18c3506e593206
Mikegv <xmik3yvx@live.com>
2022-04-27 13:52:51 -0700

    feat(news):added collapsible to news section

--- src/components/collapsible/Collapsible.js
+++ src/components/collapsible/Collapsible.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Collapsible.css'
 import Newsstorycard from '../newsstorycard/Newsstorycard'
+import DownArrowImage from '../../images/arrow-down.svg'
 
 const Collapsible = () => {
+    const [collapsed, setCollapsed] = useState(true)
+    const clickHandler = () => setCollapsed(() => !collapsed)
+    let collapsetestClass = collapsed ? 'collapsableArea' : 'collapsableArea showCollapsableArea'
+    let collapseButtonClass = collapsed ? 'openButton' : 'openButton closeButton'
     return (
         <div>
-            Collapse
+            <div className='topOfCollapsible'>
             <Newsstorycard />
+            <Newsstorycard />
+            <Newsstorycard />
+            </div>
+            <button className={collapseButtonClass} onClick={clickHandler}>
+            <img src={DownArrowImage}  alt='open collapse button' />
+            </button>
+            <div className={collapsetestClass}>
+            <Newsstorycard />
+            <Newsstorycard />
+            <Newsstorycard />
+
+            <Newsstorycard />
+            <Newsstorycard />
+            <Newsstorycard />
+            </div>
+
         </div>
     );
 };
