abe3a797c5098167b848edb402e0d43f4ba25cb8
Mikegv <xmik3yvx@live.com>
2022-04-29 08:41:44 -0700

    chore(carousel):changed some images

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -4,6 +4,8 @@ import { Carousel } from 'react-responsive-carousel';
 import GalleryImage1 from '../../images/gallery-image-1.svg'
 import GalleryImage2 from '../../images/gallery-image-2.svg'
 import GalleryImage3 from '../../images/gallery-image-3.svg'
+import GalleryImage4 from '../../images/gallery-image-4.svg'
+
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Gallery = () => {
@@ -13,18 +15,18 @@ const Gallery = () => {
             <Carousel infiniteLoop={true} width={'1130px'}>
                 <div className='papislide'>
                     <div className='slideImage'><img src={GalleryImage1} /></div>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
+                    <div className='slideImage'><img src={GalleryImage2} /></div>
+                    <div className='slideImage'><img src={GalleryImage3} /></div>
                 </div>
                 <div className='papislide'>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
+                    <div className='slideImage'><img src={GalleryImage4} /></div>
+                    <div className='slideImage'><img src={GalleryImage3} /></div>
+                    <div className='slideImage'><img src={GalleryImage2} /></div>
                 </div>
                 <div className='papislide'>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
-                    <div className='slideImage'><img src={GalleryImage1} /></div>
+                    <div className='slideImage'><img src={GalleryImage2} /></div>
+                    <div className='slideImage'><img src={GalleryImage4} /></div>
+                    <div className='slideImage'><img src={GalleryImage3} /></div>
                 </div>
             </Carousel>
         </div>
